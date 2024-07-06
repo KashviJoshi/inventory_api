d18665a756a96de7aa17b483c85d4e82e65799dc
KASHVI JOSHI <116963136+KashviJoshi@users.noreply.github.com>
2024-07-06 19:35:38 +0530

    Update settings.py

--- store_inventory/store_inventory/settings.py
+++ store_inventory/store_inventory/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Security settings
+DEBUG = False
+ALLOWED_HOSTS = ['kashvijoshi.com']
