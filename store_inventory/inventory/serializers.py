ef62ca6538d913b138e1a8fe03b39ff655fdd997
KASHVI JOSHI <kashvijoshi1202@gmail.com>
2024-07-06 19:11:54 +0530

    Initial commit
    initial commit

--- store_inventory/inventory/serializers.py
+++
@@ -1,9 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-from .models import Cuboid
-
-class CuboidSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cuboid
-        fields = ['id', 'length', 'breadth', 'height', 'created_by', 'created_at', 'updated_at', 'area', 'volume']
-        read_only_fields = ['created_by', 'created_at', 'updated_at', 'area', 'volume']
